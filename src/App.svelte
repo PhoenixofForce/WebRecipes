15ff9153dad51074cebe9fdd0675e0fe2927e3bb
PhoenixofForce <phoenixofforce@gmail.com>
2023-06-11 13:00:33 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,152 @@
+<script>
+	import QRCode from "./QRJS.svelte"
+
+	let allowCompression = false;
+
+	let ingredientInput;
+	let recipe = {
+		name: "",
+		portions: 4,
+		time: "",
+		description: "",
+		directions: "",
+		ingredients: [],
+		tags: ""
+	}
+
+	let qrCodeInput = toAndroidFormat(recipe);
+	$: {
+		recipe;
+		allowCompression;
+		debounce();
+	}
+
+	function toAndroidFormat(recipeData) {
+		let out = {};
+
+		out["name"] = recipeData.name;
+		out["portions"] = recipeData.portions;
+		if(recipeData.time.length > 0) out["time"] = recipeData.time;
+		if(recipeData.description.length > 0) out["description"] = recipeData.description;
+		if(recipeData.directions.length > 0) out["directions"] = recipeData.directions;
+		out["ingredientCount"] = recipeData.ingredients.length;
+		if(recipeData.tags.length > 0) out["tags"] = recipeData.tags;
+
+		for(let i = 0; i < recipeData.ingredients.length; i++) {
+			out["ingredient_" + i] = recipeData.ingredients.at(i);
+		}
+		return compress(JSON.stringify(out));
+	}
+
+	function compress(string) {
+		string = string.replace(/\s+/i, " ")
+
+		var compressed = LZString.compressToEncodedURIComponent(string);
+		console.log("compression would reduce " + string.length + " to " + compressed.length);
+
+		let out = "";
+		if(allowCompression) out += (compressed.length < string.length && allowCompression)? ("c"): ("u")
+		out += (compressed.length < string.length && allowCompression)? (compressed): (string);
+		return out;
+	}
+
+	function textToIngredient(textIn) {
+		if(! textIn.match(/\d+ .+ .+/i)) return;
+
+		let parts = textIn.split(" ");
+		let amount = parseInt(parts[0]);
+		let unit = parts[1];
+
+		let name = "";
+		for(let i = 2; i < parts.length; i++) name += parts[i];
+
+		recipe.ingredients.push({amount: amount, unit: unit, name: name});
+		recipe.ingredients = recipe.ingredients;
+		ingredientInput = "";
+	}
+
+	let timer;
+	const debounce = e => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			qrCodeInput = toAndroidFormat(recipe);
+		}, 1000);
+	}
+</script>
+
+<div class="main">
+	<div class="row">
+		<div class="col">
+			<div>
+				<label for="name">Recipe Name</label>
+				<input id="name" type="text" bind:value={ recipe.name } placeholder="Recipe Name"/>
+
+				<label for="portions">Portions</label>
+				<input id="portions" type="number" bind:value={ recipe.portions } placeholder="Portions"/>
+
+				<label for="time">Cooking Time</label>
+				<input id="time" type="text" bind:value={ recipe.time } placeholder="Time" />
+
+				<label for="description">Description</label>
+				<input id="description" type="text" bind:value={ recipe.description } placeholder="Description"/>
+
+				<label for="directions">Cooking Directions</label>
+				<textarea id="directions" rows="7" bind:value={ recipe.directions } placeholder="Cooking Directions"/>
+
+				<label for="ingredients">Ingredients</label>
+				<ul>
+					{#each recipe.ingredients as ingredient, i}
+					<li>
+						{ingredient.amount + ingredient.unit + " " + ingredient.name}
+						<button class="button icon-only pull-right"
+							on:click={ e => { recipe.ingredients.splice(i, 1); recipe.ingredients = recipe.ingredients; } }>
+							x
+						 </button>
+					</li>
+				{/each}
+				</ul>
+				<input id="ingredients" bind:value={ ingredientInput } type="text" placeholder="Amount Unit Name (ex: '150 g Flour')" 
+					on:keydown={e => { if(e.key == 'Enter') textToIngredient(ingredientInput)}} />
+
+				<label for="tags">Tags</label>
+				<input id="tags" type="text" bind:value={ recipe.tags } placeholder="tag1,tag2,tag3"/>
+			</div>
+		</div>
+
+		<div class="col">
+			<QRCode bind:codeValue={ qrCodeInput } bind:useCompression={ allowCompression } squareSize=1000 />
+		</div>
+
+	</div>
+</div>
+
+
+
+<style>
+	.main {
+		width: 90%;
+		margin-left: auto;
+	  	margin-right: auto;
+	}
+
+	.row {
+		margin-top: 50px;
+	}
+
+	input, textarea{
+		margin-bottom: 8px;
+	}
+
+	.button {
+		transform: translate(0, -25%);
+		padding-top: 8px;
+	}
+
+	li {
+		margin-bottom: 16px;
+	}
+
+	label {
+		font-weight: 400;
+	}
+</style>
---
+++ src/QRJS.svelte
@@ -0,0 +1,131 @@
+<script>
+    //https://svelte.dev/repl/72d3ca877047468db8bb3a68f25a8b92?version=3.14.1
+    import { onMount } from 'svelte';
+
+    export let codeValue;
+    export let squareSize; 
+
+    export let useCompression;
+    let whiteBackGround = false;
+
+    let correctLevel = QRCode.CorrectLevel.L;
+    let charactersPerLevel = [2950, 2328, 1660, 1270]
+    //[4296, 3391, 2429, 1852]; alphanumeric
+    //[2953, 2331, 1663, 1273]; binary/bits in theory
+    //[2950, 2328, 1660, 1270]; binary/bits in praxis
+
+    let qrcode;
+    let loaded = false;
+
+    let error = "";
+
+    onMount(() => {
+        loaded = true;
+        qrcode = new QRCode("qrcode", {
+            text: codeValue,
+            width: squareSize,
+            height: squareSize,
+            colorDark : "#000",
+            colorLight : whiteBackGround? "#FFF": getComputedStyle(document.body).getPropertyValue("--color-primary-light"),
+            correctLevel : correctLevel
+        });
+    });
+
+    $: updateCode(codeValue)
+    $: setCorrectlevel(correctLevel)
+    $: setColor(whiteBackGround)
+    function setCorrectlevel(level) {
+        if(loaded) {
+            qrcode._htOption.correctLevel = valueToCorrectLevel(level); 
+            console.log(level + "=>" + qrcode._htOption.correctLevel)
+            qrcode.clear();
+            updateCode(codeValue)
+        }
+    }
+
+    function setColor(useWhite) {
+        if(loaded) {
+            qrcode._htOption.colorLight = useWhite? "#FFF": getComputedStyle(document.body).getPropertyValue("--color-primary-light");
+            qrcode.clear();
+            updateCode(codeValue)
+        }
+    }
+
+    function updateCode(text)  {
+        if(loaded) {
+            qrcode.clear();
+            try {
+                qrcode.makeCode(text);
+                error = " ";
+            } catch(exception) {
+                qrcode.makeCode("");
+                error = "Input for the qr code is too long, please reduce your text."
+            }
+        }
+    }
+
+    function valueToCorrectLevel(level) {
+        return (level == 0? 1: (level == 1? 0: (level == 2? 3: 2)));
+    }
+    </script>
+
+<div>
+    <div class="is-center is-horizontal-align"><div id="qrcode"></div></div>
+    <div class="error">
+        { error }
+    </div>
+
+    <!--
+    <div style="color: {codeValue.length > 0.7 * charactersPerLevel[correctLevel]? "red": "gray"}">
+        {
+            (codeValue.at(0) == 'c'? "compressed": "uncompressed") + " at " + codeValue.length + " characters"
+        }
+    </div>
+    -->
+
+    <div class:error={codeValue.length > charactersPerLevel[correctLevel]}>
+        <input bind:value={ correctLevel } type="range" min="0" max="3" list="values">
+        <datalist id="values">
+            <option value="0" label="Low"></option>
+            <option value="1" label="Medium"></option>
+            <option value="2" label="Quality"></option>
+            <option value="3" label="High"></option>
+        </datalist>
+        <span>
+            { "QRCodes with an CorrectnessLevel of " + correctLevel + " can hold up to " + charactersPerLevel[correctLevel] + " character (" 
+            + codeValue.length + " characters, " + (100.0 * codeValue.length) / charactersPerLevel[correctLevel] + "% used)"  }
+        </span>
+    </div>
+
+    <div>
+        <label for="compression">
+            <input id="compression" bind:checked={ useCompression } type=checkbox />
+            Use Compressing <span class="error">(Warning: unsupported by the App)</span>
+        </label>
+    </div>
+
+    <div>
+        <label for="background">
+            <input id="background" bind:checked={ whiteBackGround } type=checkbox />
+            White Background for the QR Code(might help with scanning)
+        </label>
+    </div>
+</div>
+
+<style>
+    #qrcode {
+        max-width: 500px;
+        width:100%;
+        aspect-ratio: 1;
+    }
+
+    .error {
+        color: var(--color-error);
+        font-weight: bold;
+    }
+
+    div div {
+        margin-bottom: 8px;
+    }
+</style>
+
